27ed4ed6d749ee5135424712a0f464a45714513f
Opilane TTHK <opilane@tthk.ee>
2022-12-01 10:07:26 +0200

    migration error

---
+++ Shop/Shop/Controllers/SpaceshipsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models.Spaceship;
+
+namespace Shop.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly ShopContext _context;
+
+        public SpaceshipsController
+            (
+                ShopContext context
+            )
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    Type = x.Type,
+                    Passengers = x.Passengers,
+                    EnginePower = x.EnginePower,
+                });
+              
+
+            return View();
+        }
+    }
+}
--- Shop/Shop/Program.cs
+++ Shop/Shop/Program.cs
@@ -5,11 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ShopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-builder.Services.AddDbContext<ShopContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
